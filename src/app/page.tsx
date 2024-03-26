09c72b24b6c18aa84cf1a5a51c8dd0f183a202da
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-03-26 07:39:49 -0600

    transaction and history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Header from "../header/header.js";
 import History from "../history/history.js";
+import Add from "../add/add.js";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Header />
+      <History />
+      <Add />
       </main>
 
   );

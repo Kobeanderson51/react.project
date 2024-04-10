87729836d8d383a56ac93a7e98f9314782f8bf27
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-04-10 07:33:25 -0600

    fixed deposit and expense

--- src/modal/modal.tsx
+++ src/modal/modal.tsx
@@ -43,7 +43,7 @@ export function ExpenseModal({ toggleModal, isOpen}: {toggleModal: () => void, i
       >
         <FocusLock>
             <div className="ml-7">
-            <h3 id="expense"className="text-2xl p-5 ml-14">New Expense</h3>
+            <h3 id="expense"className="text-2xl p-5 ml-14">New Deposit</h3>
                 <div className="mt-6">
                     <input type="text" className="w-80 p-2 m-2 border-2 border-slate-400 rounded-md" placeholder="Enter Transaction Name" ref={nameRef} />
                     <input type="number" className="w-80 p-2 m-2 border-2 border-slate-400 rounded-md" placeholder="Enter Transaction Amount" ref={amountRef} />
@@ -58,42 +58,45 @@ export function ExpenseModal({ toggleModal, isOpen}: {toggleModal: () => void, i
     </>
   );
 }
-export function DepositModal({ toggleModal, isOpen }) {
-  const { addDeposit } = useContext(NameAndAmountDataContext);
-  const nameRef = useRef();
-  const amountRef = useRef();
+export function DepositModal({ toggleModal, isOpen}: {toggleModal: () => void, isOpen: boolean}){
 
-  const onClickAddDeposit = () => { 
-    const name = nameRef.current?.value;
-    const amount = amountRef.current?.value;
-    addDeposit({ name, amount }); 
+  const { addDeposit } = useContext(NameAndAmountDataContext);
+
+  const nameRef = useRef<HTMLInputElement>();
+  const amountRef = useRef<HTMLInputElement>();
+
+  const onClickAdd = () => {
+    const name: string = nameRef.current?.value ?? ''; // Add null check for nameRef.current
+    const amount: number = Number(amountRef.current?.value ?? 0) ?? 0;
+    const id = uuidv4()
+    addDeposit({ id, name, amount });
     toggleModal();
-  }
+  }  
   
-    return (
-      <>
-        <button className="text-3xl"onClick={toggleModal}>+</button>
-        <StyledModal
-          isOpen={isOpen}
-          onEscapeKeydown={toggleModal}
-          role="dialog"
-          aria-modal={true}
-          aria-labelledby="modal-label"
-        >
-          <FocusLock>
-              <div className="ml-7">
-              <h3 id="deposit"className="text-2xl p-5 ml-14">New Deposit</h3>
-                  <div className="mt-6">
-                      <input id="deposit-transation-name"type="text" className="w-80 p-2 m-2 border-2 border-slate-400 rounded-md" placeholder="Enter Transaction Name" ref={nameRef}/>
-                      <input id="deposit-transation-amount"type="number" className="w-80 p-2 m-2 border-2 border-slate-400 rounded-md" placeholder="Enter Transaction Amount" ref={amountRef}/>
-                  </div>
-                  <div className="mt-14">
-                      <button className="text-1xl bg-slate-400 rounded-md px-3 ml-5" onClick={toggleModal}>Cancel</button>
-                      <button className="text-1xl ml-40 bg-blue-400 rounded-md px-3" onClick={onClickAddDeposit} id="add-deposit">Add</button>
-                  </div>
-              </div>
-          </FocusLock>
-        </StyledModal>
-      </>
-    );
+  return (
+    <>
+      <button className="text-3xl" onClick={toggleModal}>+</button>
+      <StyledModal
+        isOpen={isOpen}
+        onEscapeKeydown={toggleModal}
+        role="dialog"
+        aria-modal={true}
+        aria-labelledby="modal-label"
+      >
+        <FocusLock>
+          <div className="ml-7">
+            <h3 id="deposit" className="text-2xl p-5 ml-14">New Expense</h3>
+            <div className="mt-6">
+              <input id="deposit-transation-name" type="text" className="w-80 p-2 m-2 border-2 border-slate-400 rounded-md" placeholder="Enter Transaction Name" ref={nameRef} />
+              <input id="deposit-transation-amount" type="number" className="w-80 p-2 m-2 border-2 border-slate-400 rounded-md" placeholder="Enter Transaction Amount" ref={amountRef} />
+            </div>
+            <div className="mt-14">
+              <button className="text-1xl bg-slate-400 rounded-md px-3 ml-5" onClick={toggleModal}>Cancel</button>
+              <button className="text-1xl ml-40 bg-blue-400 rounded-md px-3" onClick={onClickAdd} id="add-deposit">Add</button>
+            </div>
+          </div>
+        </FocusLock>
+      </StyledModal>
+    </>
+  );
 }

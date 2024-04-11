8641e54cd465e7abbaf235fbae305746fc750a4d
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-04-11 09:36:45 -0600

    fix balance

--- src/header/header.js
+++ src/header/header.js
@@ -1,22 +1,24 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { NameAndAmountDataContext } from '@/add/nameAndAmountData';
 import "../history/history";
+import { get } from 'http';
 
 export default function Header() {
   const { transactions = [], deposits = [] } = useContext(NameAndAmountDataContext);
 
   const transactionsTotal = parseFloat(transactions.reduce((acc, item) => (acc += Number(item.amount)), 0)).toFixed(2);
   const depositsTotal = parseFloat(deposits.reduce((acc, item) => (acc += Number(item.amount)), 0)).toFixed(2);
-  const total = transactionsTotal - depositsTotal;
+  const total = (transactionsTotal - depositsTotal)
+  const displayTotal = new Intl.NumberFormat('en-US', {
+    currency: 'USD',
+    style: 'currency',
+    minimumFractionDigits: 2,
+  }).format(
+    total,
+  )
   
-  const displayAmount = document.getElementById('display-amount');
-  if (total < 0) {
-    displayAmount?.classList.remove('text-green-500')
-    displayAmount?.classList.add('text-red-400')
-  } else {
-    displayAmount?.classList.remove('text-red-400')
-    displayAmount?.classList.add('text-green-500')
-  } 
+
+  const textColor = total < 0 ? 'text-red-400' : 'text-green-500';
 
   return (
       <>
@@ -25,7 +27,7 @@ export default function Header() {
         <h1 className="text-5xl p-5">Expense Tracker</h1>
         <h2></h2>
           <h2 className="text-3xl p-5">Your Balance</h2>
-            <h3 className="text-3xl p-5"id='display-amount'>${total}</h3>
+            <h3 className={`text-3xl p-5 ${textColor}`}  id='display-amount'>{displayTotal}</h3>
               <h3></h3>
             <div className="flex flex-row justify-center bg-slate-300 shadow-lg shadow-slate-400 hover:shadow-2xl">
               <div className="p-5">

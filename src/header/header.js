43b8b9d839ba6207a6925bfafbdef9480983c01a
Evan.Cortez <EVAN.CORTEZ3145@STU.MTEC.EDU>
2024-04-11 08:30:26 -0600

    header mods

--- src/header/header.js
+++ src/header/header.js
@@ -9,6 +9,14 @@ export default function Header() {
   const depositsTotal = parseFloat(deposits.reduce((acc, item) => (acc += Number(item.amount)), 0)).toFixed(2);
   const total = transactionsTotal - depositsTotal;
   
+  const displayAmount = document.getElementById('display-amount');
+  if (total < 0) {
+    displayAmount.classList.remove('text-green-500')
+    displayAmount.classList.add('text-red-400')
+  } else {
+    displayAmount.classList.remove('text-red-400')
+    displayAmount.classList.add('text-green-500')
+  }
 
   return (
       <>
@@ -17,17 +25,17 @@ export default function Header() {
         <h1 className="text-5xl p-5">Expense Tracker</h1>
         <h2></h2>
           <h2 className="text-3xl p-5">Your Balance</h2>
-            <h3 className="text-3xl text-green-500 p-5"id='display-amount'>${total}</h3>
+            <h3 className="text-3xl p-5"id='display-amount'>${total}</h3>
               <h3></h3>
             <div className="flex flex-row justify-center bg-slate-300 shadow-lg shadow-slate-400 hover:shadow-2xl">
               <div className="p-5">
                 <h3 className='text-black'>INCOME</h3>
-                <p className="text-green-500">+$0.00</p>
+                <p className="text-green-500">+${transactionsTotal}</p>
                 </div>
                 <div className="text-3xl flex items-center text-gray-400">|</div>
                 <div className="p-5">
                 <h3 className='text-black'>EXPENSE</h3>
-                <p className="text-red-400">-$0.00</p>
+                <p className="text-red-400">-${depositsTotal}</p>
                 </div>
             </div>
         </header>
